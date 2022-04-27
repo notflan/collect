9ecd0ea3d84026ef28f50743b4acf1f057082e8b
Avril <flanchan@cumallover.me>
2022-04-27 21:57:29 +0100

    Fixed silly bug preventing building on rust-stable.
    Fortune for collect's current commit: Small blessing − 小吉

--- src/memfile/map.rs
+++ src/memfile/map.rs
@@ -10,13 +10,21 @@ use libc::{
 };
 
 //TODO: Make this a `bitflags` struct.
-#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Copy, Default)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Copy)]
 #[repr(i32)]
 pub enum MapProtection
 {
-    #[default]
     None = PROT_NONE,
     Read = PROT_READ,
     Write = PROT_WRITE,
     Execute = PROT_EXEC,
 }
+
+impl Default for MapProtection
+{
+    #[inline(always)]
+    fn default() -> Self
+    {
+	Self::None
+    }
+} 
